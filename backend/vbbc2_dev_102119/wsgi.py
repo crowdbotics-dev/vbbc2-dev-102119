c715cabf3fb6f63a3ba1192d9177eec603b79abd
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 07:36:46 +0000

    Initial commit.

---
+++ backend/vbbc2_dev_102119/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for vbbc2_dev_102119 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vbbc2_dev_102119.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vbbc2-dev-102119",
+  url: "https://vbbc2-dev-102119.botics.co",
+  api: "https://vbbc2-dev-102119.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
